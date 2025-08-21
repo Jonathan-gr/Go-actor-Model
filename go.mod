2b78586addccfb38d9d15873e9292c826affdc88
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2025-08-22 01:47:57 +0300

    actor spawned

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Jonathan-gr/Go-actor-Model
 
 go 1.23.2
+
+require (
+	github.com/DataDog/gostackparse v0.7.0 // indirect
+	github.com/anthdm/hollywood v1.0.5 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/anthdm/hollywood/actor"
+)
+
+type Player struct {
+	HP int
+}
+
+func newPlayer() actor.Receiver {
+	return &Player{}
+}
+
+func (p *Player) Receive(c *actor.Context) {}
 
 func main() {
-	fmt.Println("Hello, actors")
+	e, err := actor.NewEngine(actor.NewEngineConfig())
+	if err != nil {
+		log.Fatalf("failed to create actor engine: %v", err)
+	}
+	e.Spawn(newPlayer, "player")
 }
