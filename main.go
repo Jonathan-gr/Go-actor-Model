f0a3b8ab08331323cabd58176b3b0ef8f3bf232f
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2025-08-24 23:51:35 +0300

    cases for player

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/anthdm/hollywood/actor"
@@ -16,12 +17,19 @@ func newPlayer(hp int) actor.Producer {
 	}
 }
 
-func (p *Player) Receive(ctx *actor.Context) {
+type takeDamage struct{}
+
+func (p *Player) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case takeDamage:
+		fmt.Println("Taking damage!", msg)
+	}
 }
 func main() {
 	e, err := actor.NewEngine(actor.NewEngineConfig())
 	if err != nil {
 		log.Fatal(err)
 	}
-	e.Spawn(newPlayer(100), "player1")
+	pid := e.Spawn(newPlayer(100), "player1", actor.WithID("actor12323"))
+	fmt.Println(pid)
 }
