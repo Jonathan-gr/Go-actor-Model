5fdece3e160b1d2083a92892c043605de6c668b2
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2025-08-25 23:52:05 +0300

    poison pill to end actor

--- main.go
+++ main.go
@@ -1,28 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/anthdm/hollywood/actor"
 )
 
-type Player struct {
-	HP int
+type Inventory struct {
+	Bottles int
 }
 
+type Player struct {
+	HP           int
+	inverntoryID *actor.PID
+}
+
+func newInventory(bottles int) actor.Producer {
+	return func() actor.Receiver {
+		return &Inventory{Bottles: bottles}
+	}
+}
 func newPlayer(hp int) actor.Producer {
 	return func() actor.Receiver {
 		return &Player{HP: hp}
 	}
 }
 
-type takeDamage struct{}
+type drinkBottle struct {
+	Amount int
+}
+
+func (i *Inventory) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Started:
+		fmt.Println("Inventory started with bottles:", i.Bottles)
+	case actor.Stopped:
+		fmt.Println("Inventory stopped")
+	case drinkBottle:
+		fmt.Println("Drinking bottle, healing for", msg.Amount)
+	}
+}
 
 func (p *Player) Receive(c *actor.Context) {
-	switch msg := c.Message().(type) {
-	case takeDamage:
-		fmt.Println("Taking damage!", msg)
+	switch c.Message().(type) {
+	case actor.Started:
+		fmt.Println("Player started with HP:", p.HP)
+		// Spawn an inventory actor as a child
+		p.inverntoryID = c.SpawnChild(newInventory(3), "inventory")
+	case actor.Stopped:
+		fmt.Println("Player stopped")
+	case drinkBottle:
+		c.Forward(p.inverntoryID)
 	}
 }
 func main() {
@@ -31,5 +62,10 @@ func main() {
 		log.Fatal(err)
 	}
 	pid := e.Spawn(newPlayer(100), "player1", actor.WithID("actor12323"))
-	fmt.Println(pid)
+
+	msg := drinkBottle{Amount: 10}
+	e.Send(pid, msg)
+
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	<-e.PoisonCtx(ctx, pid).Done()
 }
